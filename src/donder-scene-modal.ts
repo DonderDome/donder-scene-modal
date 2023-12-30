8f494ac317e1bf344a4cbf8e50015ea214f49212
Muffles <ruimolefas@Ruis-MBP.lan>
2023-12-30 15:19:26 +0000

    build

--- src/donder-scene-modal.ts
+++ src/donder-scene-modal.ts
@@ -364,6 +364,7 @@ export class BoilerplateCard extends LitElement {
   }
 
   protected closeModal() {
+    console.log(this.config)
     if (this.config.closeModal) {
       this.config.closeModal();
     } else {
